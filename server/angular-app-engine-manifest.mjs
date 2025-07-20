90dbbc8775f584f7616fdda35aae8a6424b89d82
Tejkiran123 <kavalitejkiran123@gmail.com>
2025-07-20 23:09:32 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: 'https://tejkiran123.github.io/kiranprofile',
+  basePath: '/latestPortfolio',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'https://tejkiran123.github.io/kiranprofile/',
+  baseHref: '/latestPortfolio/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/kiranprofile"
+    "route": "/latestPortfolio"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 538, hash: 'c2ba801ea9c3165f0cfbf0c54116f089d36cde1ec7c8a0ea468607994d45fe9d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1051, hash: '80f1bac2b1f217b3ab0467ea0d4e23e93f921f6d6e19bf768bd942d9debd83ae', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 12494, hash: 'c097270b9271faae23587633bdba4cdf5b03f2e239640c002eb5506c6b7c07f9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 512, hash: 'e8b0b71bd8e5337d4f29395fb09e6c0ae3321ec58d9e9ac46e8abe3c6ec22b94', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1025, hash: 'ee1bea065cd8aee35e4de69a7cde5b3dac9f61d2a6e38a98d6a277022050dc96', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 12468, hash: '5ef085e99386341a8815b4a613d91fd5d195889443a53825050c8534e6e5bec7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
